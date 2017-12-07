e4896dc51263647acfe3c9e4bf5e871b0f82eafd
HumbleNarcissus <maciek.tarach06@gmail.com>
2017-12-07 22:44:30 +0100

    first commit

---
+++ js/game.js
@@ -0,0 +1,11 @@
+var game = new Phaser.Game(640, 480, Phaser.AUTO, '');
+
+//game states
+game.state.add('play', playState);
+game.state.add('load', loadState);
+game.state.add('endMenu', endMenuState);
+game.state.add('menu', menuState);
+
+
+//starting game
+game.state.start('load')
